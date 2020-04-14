b6e01a81cd68b5fcb966dabc0b8085215dba6d0e
sachiko <sachicosworks@gmail.com>
2020-04-14 13:12:55 +0000

    finish chapter3

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+  def home
+    # => app/views/#{リソース名}/@{アクション名}.html.erb
+  end
+
+  def help
+  end
+  
+  def about
+    # => app/views/static_pages/about.html.erb
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'static_pages/home'
   # => StaticPages#home
   get 'static_pages/help'
   # => StaticPages#help
-  root 'application#hello'
+  get 'static_pages/about'
 end
