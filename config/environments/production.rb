e257a677b183893484bc05783d80177c2be7ee65
sachiko <sachicosworks@gmail.com>
2020-05-10 13:01:28 +0000

    change config heroku-mailer user/password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app167737162@heroku.com'],
-    :password       => ENV['mykbcthu4044'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
